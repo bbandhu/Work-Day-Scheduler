57e0f104e68007d617307b818e4d61f6f1171b3d
bandhavi <bandhavi.bendi@coupa.com>
2023-03-16 20:19:22 -0700

    redone

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ After Deployment, the work Scheduler application should be loaded successfully w
 
 ## Tools used
 CSS, Html , JS , Jquery
-
+JS 
+This JavaScript code includes functions to get the current date, dynamically generate time blocks for standard business hours, compare the current time to time blocks and dynamically update their color coding, load saved events from local storage, and save event text to local storage when the save button is clicked. Note that this code uses the Moment.js library to manipulate and format dates and times, so you will need to include the Moment.js library in your HTML file. Additionally, the IDs and classes used in this code will need to correspond with those used in the HTML and CSS files to ensure proper functionality.
 
 ## Project 
 -Workday Schduler has rows from 9 -5 time and save button and section displays the local storage and current day and year.
--- script.js
+++ script.js
@@ -1,63 +1,52 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var save = $('.saveBtn');
-var savedTime = $('#ScheduledTime');
-$(document).ready(function(){
-  save.on('click', function(){
-        var value = $(this).siblings('.description').val();
-        var time = $(this).parent().attr('id');
-      localStorage.setItem(time, value);
-      //display the time is saed to local storage
-      savedTime.addClass('display');
-   })
+// Get the current date and display it in the planner header
+var currentDate = moment().format("dddd, MMMM Do YYYY");
+$("#currentDay").text(currentDate);
 
-  // Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  function updateTime(){
-    var currentTime = dayjs().hour();
+// Dynamically generate time blocks for standard business hours
+for (var i = 9; i <= 17; i++) {
+  var blockTime = moment().hour(i).format("hA");
+  var blockHTML = "<div class='time-block' id='block-" + i + "'>" +
+                    "<div class='hour'>" + blockTime + "</div>" +
+                    "<textarea class='event'></textarea>" +
+                    "<button class='save-button'>Save</button>" +
+                  "</div>";
+  $("#planner").append(blockHTML);
+}
 
-  // loop over time blocks
-  $('.time-block').each(function () {
-    var blockedTime = parseInt($(this).attr('id').split('-')[1]);
-
-    // check if we've moved past this time
-    if (blockedTime < currentTime) {
-      $(this).addClass('past');
-    } else if (blockedTime === currentTime) {
-      $(this).removeClass('past');
-      $(this).addClass('present');
+// Compare current time to time blocks and dynamically update color coding
+function updateBlocks() {
+  var currentHour = moment().hour();
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
     } else {
-      $(this).removeClass('past');
-      $(this).removeClass('present');
-      $(this).addClass('future');
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
     }
   });
+}
+
+// Call updateBlocks function to initialize color coding and update every hour
+updateBlocks();
+setInterval(updateBlocks, 3600000);
+
+// Load saved events from local storage
+$(".event").each(function() {
+  var blockID = $(this).parent().attr("id");
+  var savedEvent = localStorage.getItem(blockID);
+  if (savedEvent !== null) {
+    $(this).val(savedEvent);
   }
+});
 
-  updateTime();
-
-  // Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
-// load any saved data from localStorage
-$('#hour-9 .description').val(localStorage.getItem('hour-9'));
-$('#hour-10 .description').val(localStorage.getItem('hour-10'));
-$('#hour-11 .description').val(localStorage.getItem('hour-11'));
-$('#hour-12 .description').val(localStorage.getItem('hour-12'));
-$('#hour-13 .description').val(localStorage.getItem('hour-13'));
-$('#hour-14 .description').val(localStorage.getItem('hour-14'));
-$('#hour-15 .description').val(localStorage.getItem('hour-15'));
-$('#hour-16 .description').val(localStorage.getItem('hour-16'));
-$('#hour-17 .description').val(localStorage.getItem('hour-17'));
-
-// Add code to display the current date in the header of the page.
-// display current day on page
-$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
-
+// Save event text to local storage when save button is clicked
+$(".save-button").on("click", function() {
+  var blockID = $(this).parent().attr("id");
+  var eventText = $(this).siblings(".event").val();
+  localStorage.setItem(blockID, eventText);
 });
